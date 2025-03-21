e728f74a0c0ba1d586157f0d6d19b753695c23b4
KIA <mojtabahosseinikia@gmail.com>
2025-03-21 15:41:28 +0330

    main

---
+++ .next/server/_rsc_content_index_mdx_metadata.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_content_index_mdx_metadata";
+exports.ids = ["_rsc_content_index_mdx_metadata"];
+exports.modules = {
+
+/***/ "(rsc)/./content/index.mdx?metadata":
+/*!************************************!*\
+  !*** ./content/index.mdx?metadata ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   metadata: () => (/* binding */ metadata)\n/* harmony export */ });\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ \"(rsc)/./node_modules/.pnpm/next@15.2.3_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-runtime.js\");\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst metadata = {\n    \"title\": \"سلام!\",\n    \"filePath\": \"content/index.mdx\",\n    \"timestamp\": 1742558582798\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9jb250ZW50L2luZGV4Lm1keD9tZXRhZGF0YSIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBcUU7QUFDOUQsTUFBTUksV0FBVztJQUN0QixTQUFTO0lBQ1QsWUFBWTtJQUNaLGFBQWE7QUFDZixFQUFFIiwic291cmNlcyI6WyIvVXNlcnMvbW9qdGFiYS9Eb2N1bWVudHMvR2l0SHViL2RvL2NvbnRlbnQvaW5kZXgubWR4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7RnJhZ21lbnQgYXMgX0ZyYWdtZW50LCBqc3ggYXMgX2pzeH0gZnJvbSBcInJlYWN0L2pzeC1ydW50aW1lXCI7XG5leHBvcnQgY29uc3QgbWV0YWRhdGEgPSB7XG4gIFwidGl0bGVcIjogXCLYs9mE2KfZhSFcIixcbiAgXCJmaWxlUGF0aFwiOiBcImNvbnRlbnQvaW5kZXgubWR4XCIsXG4gIFwidGltZXN0YW1wXCI6IDE3NDI1NTg1ODI3OThcbn07XG4iXSwibmFtZXMiOlsiRnJhZ21lbnQiLCJfRnJhZ21lbnQiLCJqc3giLCJfanN4IiwibWV0YWRhdGEiXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(rsc)/./content/index.mdx?metadata\n");
+
+/***/ })
+
+};
+;
