c7a51bec4d8567a6cbc53992d307e3202100cb3b
KIA <mojtabahosseinikia@gmail.com>
2025-03-22 13:21:50 +0330

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -56,7 +56,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          // docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/unKia/do"
           // footer={footer}
           search={search}
           editLink=''
