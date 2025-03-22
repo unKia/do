b9a7933ab232145ca5df5a3392301c1cc4b068c2
KIA <mojtabahosseinikia@gmail.com>
2025-03-22 13:01:56 +0330

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import { NotFoundPage } from 'nextra-theme-docs'
  
 export default function NotFound() {
   return (
-    <NotFoundPage>
+    <NotFoundPage content="" labels="">
       <h1>۴۰۴ | صفحه موردنظر یافت نشد.</h1>
     </NotFoundPage>
   )
