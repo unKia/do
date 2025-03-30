54bd080590caced59c3aac97615a5fbc7fda13e4
KIA <mojtabahosseinikia@gmail.com>
2025-03-30 23:13:31 +0330

    main

--- app/layout.jsx
+++ app/layout.jsx
@@ -70,7 +70,6 @@ export default async function RootLayout({ children }) {
               theme: {
                 timestamp: false,
                 pagination: false,
-
               }
             }} },
             {
