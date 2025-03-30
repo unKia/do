47304cd396fdf1d51d4f459b441057fc06ef14ba
KIA <mojtabahosseinikia@gmail.com>
2025-03-30 15:12:59 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
 import nextra from 'nextra'
  
-const withNextra = nextra({
-  
-})
- 
-// You can include other Next.js configuration options here, in addition to Nextra settings:
-export default withNextra({
-  // ... Other Next.js config options
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true // mandatory, otherwise won't export
   }
   // Optional: Change the output directory `out` -> `dist`
   // distDir: "build"
+}
+const withNextra = nextra({
+  // ... other Nextra config options
 })
+ 
+export default withNextra(nextConfig)
